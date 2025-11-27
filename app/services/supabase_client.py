c3adc41743684d2945c74e5b4bf883ec4e775123
Zeracc <nicolascavalcante.rosa@outlook.com>
2025-11-27 17:39:28 -0300

    fix db connection expose

--- app/services/supabase_client.py
+++ app/services/supabase_client.py
@@ -1,12 +1,12 @@
-import os
+
 from supabase import create_client, Client
 from dotenv import load_dotenv
+import os
 
 load_dotenv()  # carrega .env
 
-
-SUPABASE_URL = "https://ylugeisisejxkhxawzlh.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsdWdlaXNpc2VqeGtoeGF3emxoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MjE3MzQ5NywiZXhwIjoyMDc3NzQ5NDk3fQ.zOf53I2QPtsw5HF5o81vEo5rQHDwXEYYNk13atRLDSM"
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise RuntimeError("Defina SUPABASE_URL e SUPABASE_KEY no .env")
